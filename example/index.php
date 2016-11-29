93d003f2022ffbf825b0c29d842fa15f86399e3a
lucas cordeiro da Silva <lukas_civil@hotmail.com>
2016-11-29 11:16:49 -0200

    Retesting
    retesting

--- example/index.php
+++ example/index.php
@@ -8,8 +8,8 @@
 
     $struct1 = utf8_encode(file_get_contents('json/json1.json'));
     $struct2 = json_decode(utf8_encode(file_get_contents('json/json2.json')), true);
-    $struct3 = array("casa"=>1, "b"=>"5", "cafeina"=>array("ss"=>"ddd"), "oi"=>5, "1" => "255");
-    $struct4 = array("casa"=>2, "cafeina"=>array("ss"=>array("ff"=>22)), "oi2"=>5, "1"=>"", "ss"=>"dddddf");
+    $struct3 = array("casa"=>1, "b"=>"5", "cafeina"=>array("ss1"=>"1", "ss2"=>"2"), "oi"=>5, "1" => "255");
+    $struct4 = array("casa"=>2, "cafeina"=>array("ss"=>array("ff"=>21, "ff1"=>22)), "oi2"=>5, "1"=>"", "ss"=>"dddddf");
 
     $struct5 = new stdClass();
     $struct5->oi = "s55";
@@ -20,7 +20,7 @@
         public $cafe = "frio";
     }
 
-    class classstruct2 extends classstruct1{
+    class classstruct2 extends classstruct1 {
         public $struct4cc = "2";
     }
 
@@ -30,26 +30,33 @@
     $struct8 = $struct3;
     $struct8["cafeina"]["ss"] = new classstruct2();
 
-    //getdiff(modified struct, static struct)
-    echo "\ngetdiff(modified struct, static struct)<br/>\n";
-    print_r($treewalker->getdiff($struct4, $struct3));
+    //getdiff(modified struct, static struct, slashtostruct) -------------------------------
+    //slashtostruct can be true or false
+    echo "\ngetdiff(modified struct, static struct, slashtostruct)<br/>\n";
+    print_r($treewalker->getdiff($struct1, $struct2, true));
     echo "<br/><br/>\n\n";
 
-    //replaceValues(struct, newvalue, known key, (boolean)change all the keys values found if the occurrence be a leaf)
-    /**
-     * {"cafeina": "oi"} -> leaf
-     * {"cafeina": "child": {"name": "Lucas"}} -> non-leaf
-     */
-    echo "replaceValues(struct, newvalue, known key, isleaf)<br/>\n";
-    print_r($treewalker->replaceValues($struct4, "test", "ss", false));
+    //walker(struct, function) -------------------------------------------------------------
+    echo "walker(struct, function)<br/>\n";
+    print_r($treewalker->walker($struct4, function(&$struct, $key, &$value) {
+        //Removing element
+        if ($key == "ff") {
+            unset($struct[$key]);
+        }
+
+        //changing element
+        if ($key == "ff1") {
+            $value = array("son" => "tiago");
+        }
+    }));
     echo "<br/><br/>\n\n";
 
-    //createDynamicallyObjects(struct, newObjectPath)
+    //createDynamicallyObjects(struct, newObjectPath) ---------------------------------------
     echo "createDynamicallyObjects(struct, newObjectPath)<br/>\n";
-    print_r($treewalker->createDynamicallyObjects($struct5, array("dd", 2, 5, 9, 10, 11)));
+    print_r($treewalker->createDynamicallyObjects($struct3, array("cafeina", "novo")));
     echo "<br/><br/>\n\n";
 
-    //getDynamicallyValue(struct, static)
+    //getDynamicallyValue(struct, static) ---------------------------------------------------
     echo "getDynamicallyValue(struct, static)<br/>\n";
 
     echo "Static access<br/>\n";
@@ -58,5 +65,24 @@
     echo "\n<br/>Dynamic access<br/>\n";
     $dynamicpath = array("cafeina","ss");
     print_r($treewalker->getDynamicallyValue($struct4, $dynamicpath)); // Dynamic access
+    echo "<br/>\n\n";
+
+    //setDynamicallyValue(struct, static, value) --------------------------------------------
+    echo "setDynamicallyValue(struct, static, value)<br/>\n";
+
+    echo "Static access<br/>\n";
+    $struct4["cafeina"]["ss"] = "newvalue";// Static access
+    print_r($struct4);
+
+    $struct4["cafeina"]["ss"] = "";
+
+    echo "\n<br/>Dynamic access<br/>\n";
+    $dynamicpath = array("cafeina","ss");
+    print_r($treewalker->setDynamicallyValue($struct4, $dynamicpath, "newvalue")); // Dynamic access
+    echo "<br/>\n\n";
+
+    //structMerge(struct, static, slashtostruct) --------------------------------------------
+    echo "\n<br/>structMerge<br/>\n";
+    print_r($treewalker->structMerge($struct4, $struct3, true));
     echo "<br/>\n\n";
 ?>
